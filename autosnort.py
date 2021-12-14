fc908b71a2a0dfec6daf5f24ba6eef6a62f14fc5
Ryan Thomsen <ryanthomsen.co@gmail.com>
2021-12-13 20:20:48 -0800

    Update autosnort.py
    Added Basic File Reading from Linux Command Line

--- autosnort.py
+++ autosnort.py
@@ -1,1 +1,22 @@
-#Great Endings have Small Beginnings
+#!/usr/bin/env python3
+
+### IMPORT STATEMENTS ###
+import sys
+
+
+### HELPER FUNCTIONS (IF NECESSARY) ###
+def HelperMethods(file):
+  lines_list = file.readlines()
+
+
+### MAIN FUNCTION ###
+def main():
+  file_name = sys.argv[1]
+  open_file = open(file_name)
+  HelperMethods(open_file)
+  open_file.close()
+
+
+### DUNDER CHECK ###
+if __name__ == "__main__":
+  main()
