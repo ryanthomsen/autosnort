3075be7f41c3f32726575d532c930b2a13be5249
Ryan Thomsen <ryanthomsen.co@gmail.com>
2021-12-15 19:27:26 -0800

    Update autosnort.py

--- autosnort.py
+++ autosnort.py
@@ -2,19 +2,32 @@
 
 ### IMPORT STATEMENTS ###
 import sys
+import os
+import scapy.all as scapy
 
 
 ### HELPER FUNCTIONS (IF NECESSARY) ###
-def HelperMethods(file):
-  lines_list = file.readlines()
-
+def HelperMethods(pcap):
+  for i in range(0,20):
+    print(str(i) + " : ")
+    pcap[i].summary
+    print("\n\n")
+  
 
 ### MAIN FUNCTION ###
 def main():
-  file_name = sys.argv[1]
-  open_file = open(file_name)
-  HelperMethods(open_file)
-  open_file.close()
+  #file_name = sys.argv[1]
+  file_name = "Project test.pcapng"
+  # Check if pcap file exists
+  # if os.path.isfile(file_name):
+  if os.path.isfile(file_name):
+    scapy_cap = scapy.rdpcap(file_name)
+  else:
+    print("Error:", file_name, "doesn't not exist.")
+    sys.exit(1)
+  #open_file = open(file_name)
+  HelperMethods(scapy_cap)
+  #open_file.close()
 
 
 ### DUNDER CHECK ###
