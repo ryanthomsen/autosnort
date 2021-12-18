20baceb3b66c323bdd5c25b522d71c33df34f097
Ryan Thomsen <ryanthomsen.co@gmail.com>
2021-12-18 11:48:02 -0800

    Update autosnort.py
    Added rule generation for ports

--- autosnort.py
+++ autosnort.py
@@ -12,64 +12,74 @@ import getopt
 
 #MAGIC NUMBERS
 SID_START = 1000000
+BAD_PORTS = [1337, 666, 31, 1170, 1234, 1243, 1981, 2001, 2023, 2140, 2989, 3024, 3150, 3700, 4950, 6346, 6400, 6667, 6670, 12345,
+             12346, 16660, 18753, 20034, 20432, 20433, 27374, 27444, 27665, 30100, 31335, 31337, 33270, 33567, 33568, 40421, 60008, 65000]
 
-### HELPER FUNCTIONS (IF NECESSARY) ###
+### HELPER FUNCTIONS ###
 def readP(singlepacket):
-  # Print IP Layer Rules
+  # Print IP source and destination
 	# Check if the IP layer is present in the packet
   if IP in singlepacket:
     ipsource = singlepacket[IP].src
     ipdest = singlepacket[IP].dst
     print("IP Source: " + str(ipsource) + " | IP Dest: " + str(ipdest))
 
+  # Print TCP source port and destination port
+  # Check if TCP is present in the packet
   if TCP in singlepacket:
     tcpsourceport = singlepacket[TCP].sport
     tcpdestport = singlepacket[TCP].dport
     print("TCP Source Port: " + str(tcpsourceport) + " | TCP Dest Port: " + str(tcpdestport))
 
-
+    # Print HTTP Request Type
+    # Check if HTTP is present in the packet
     if tcpdestport == 80:
       if singlepacket.haslayer(HTTPRequest):  # Checks if packet has payload
         #rawload = singlepacket[0][0][Raw].load
         print("HTTP Request Type: " + str(singlepacket[HTTPRequest].Method))
 
-
+  # Print DNS Hostname
+	# Check if DNS is present in the packet
   if DNS in singlepacket:
     hostname = singlepacket[DNSQR].qname
     print("DNS HostName: " + str(hostname))
 
+  # Print DNS Host Address
+  # Check if Scapy is able to load additional DNS information
   if DNSRR in singlepacket:
     hostaddr = singlepacket[DNSRR].rdata
     #recordtype = singlepacket[DNSRR].qtype
     print("DNS Host Address: " + str(hostaddr)) #+ " | TCP Dest Port: " + str(recordtype))
   
+  # Print UDP source port and UDP destination port
+  # Check if UDP is present in the packet
   if UDP in singlepacket:
     udpsrcport = singlepacket[UDP].sport
     udpdestport = singlepacket[UDP].dport
     print("UDP Source Port: " + str(udpsrcport) + " | UDP Dest Port: " + str(udpdestport))
-    
+  
+  # Print ICMP Type:
+  # Check if ICMP is present in the packet
   if ICMP in singlepacket:
     icmptype = singlepacket[ICMP].type
     print("ICMP Type: " + str(icmptype))
 
-
+# Method to suggest snort rules based on packet information
 def RuleMaker(singlepacket) -> list:
   suggestion = ""
   rule_list = []
+
+  #TCP Rules
   if TCP in singlepacket:
     tcpsourceport = singlepacket[TCP].sport
     tcpdestport = singlepacket[TCP].dport
-    if tcpsourceport == 1337:
-      suggestion += "drop TCP " + \
-          singlepacket[IP].src + \
-          ' 1337 -> any any (msg: "Suspicious activity on Port 1337"; sid ' + \
-          str(SID_START + 1) + ")\n"
+
+    #Checks if suspicious ports
+    if tcpsourceport in BAD_PORTS:
+      suggestion = "drop TCP " + str(singlepacket[IP].src) + ' ' + str(tcpsourceport) +' -> any any (msg: "Suspicious activity on Port ' + str(tcpsourceport) + '"; sid ' + str(SID_START + 1) + ")\n"
       rule_list.append(suggestion)
-    if tcpdestport == 1337:
-      suggestion += "drop TCP any any -> " + \
-          singlepacket[IP].src + \
-          ' 1337 (msg: "Suspicious Port activity on 1337"; sid ' + \
-          str(SID_START + 2) + ")\n"
+    if tcpdestport == BAD_PORTS:
+      suggestion = "drop TCP any any -> " + str(singlepacket[IP].src) + ' ' + str(tcpdestport) + ' (msg: "Suspicious Port activity on  ' + str(tcpdestport) + '"; sid ' + str(SID_START + 2) + ")\n"
       rule_list.append(suggestion)
   #Return the rules
   return rule_list
@@ -103,6 +113,7 @@ def HelperMethods(pcap):
 def main():
   #file_name = sys.argv[1]
   file_name = "1337 nc.pcap"
+  #file_name = "Project test.pcapng"
   # Check if pcap file exists
   # if os.path.isfile(file_name):
   if os.path.isfile(file_name):
