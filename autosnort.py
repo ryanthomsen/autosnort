8262ae1240e81fcbe0c482d679973a8db425ee60
Ryan Thomsen <ryanthomsen.co@gmail.com>
2021-12-18 01:56:07 -0800

    Update autosnort.py
    Added a read packet function that allows the user to see some of the data that can be viewed in Python from the pcap.

--- autosnort.py
+++ autosnort.py
@@ -3,18 +3,52 @@
 ### IMPORT STATEMENTS ###
 import sys
 import os
-import scapy.all as scapy
+from scapy.all import *
+from scapy.layers.inet import IP, UDP, TCP, ICMP
+from scapy.layers.dns import DNS, DNSQR, DNSRR
 
+import getopt
 
 ### HELPER FUNCTIONS (IF NECESSARY) ###
+def readP(singlepacket):
+  # Print IP Layer Rules
+	# Check if the IP layer is present in the packet
+  if IP in singlepacket:
+    ipsource = singlepacket[IP].src
+    ipdest = singlepacket[IP].dst
+    print("IP Source: " + str(ipsource) + " | IP Dest: " + str(ipdest))
+
+  if TCP in singlepacket:
+    tcpsourceport = singlepacket[TCP].sport
+    tcpdestport = singlepacket[TCP].dport
+    print("TCP Source Port: " + str(tcpsourceport) + " | TCP Dest Port: " + str(tcpdestport))
+  
+  if DNS in singlepacket:
+    hostname = singlepacket[DNSQR].qname
+    print("DNS HostName: " + str(hostname))
+
+  if DNSRR in singlepacket:
+    hostaddr = singlepacket[DNSRR].rdata
+    print("DNS Host Address: " + str(hostaddr))
+  
+  if UDP in singlepacket:
+    udpsrcport = singlepacket[UDP].sport
+    udpdestport = singlepacket[UDP].dport
+    print("UDP Source Port: " + str(udpsrcport) + " | UDP Dest Port: " + str(udpdestport))
+    
+  if ICMP in singlepacket:
+    icmptype = singlepacket[ICMP].type
+    print("ICMP Type: " + str(icmptype))
+
+
 def HelperMethods(pcap):
   counter = 0
-  for packet in pcap:
-    if(counter<= 100):
-      print(str(counter) + " : ")
-      print(str(packet.summary))
-      print("\n")
-      counter +=1
+  for data in pcap:
+    print(str(counter) + " : ")
+    readP(data)
+    counter +=1
+    print("\n\n")
+  
   
 
 ### MAIN FUNCTION ###
@@ -24,7 +58,7 @@ def main():
   # Check if pcap file exists
   # if os.path.isfile(file_name):
   if os.path.isfile(file_name):
-    scapy_cap = scapy.rdpcap(file_name)
+    scapy_cap = rdpcap(file_name)
   else:
     print("Error:", file_name, "doesn't not exist.")
     sys.exit(1)
