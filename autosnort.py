10ef53914cf50832b2a8679a8807febca5dd5be2
MattAges <87162303+MattAges@users.noreply.github.com>
2021-12-22 23:34:00 -0700

    Nmap scan rule groundwork
    Basics of looking for an NMAP scan.
    
    Records source IPs as a key in a dictionary, and stores dport and timestamps for each packet in a list.
    
    To do:
    
    Ignore ephemeral dst ports, finally check for packets that are too rapid, possibly store the packet number instead of the string, and add support for UDP and ICMP scans

--- autosnort.py
+++ autosnort.py
@@ -3,7 +3,7 @@
 ### IMPORT STATEMENTS ###
 import sys
 import os
-from pigget import *
+#from pigget import *
 from time import gmtime
 from scapy.all import *
 from scapy.layers.inet import IP, UDP, TCP, ICMP
@@ -19,20 +19,22 @@ BAD_PORTS = [1337, 666, 31, 1170, 1234, 1243, 1981, 2001, 2023, 2140, 2989, 3024
 include_all_packets = False
 include_PKT_numbers = False
 EPOCH = False
-Enable_WhiteList = False
+
+#Filler values
 
 #Read config rules
+scan_timing_threshold = 0.1
 
-
+#Misc vars
 packet_list = []
+pinged_port_list = {}
 
 
 ### HELPER FUNCTIONS ###
 
-def loadP(singlepacket):
+""" def loadP(singlepacket):
   custom_packet = Pigget(singlepacket)
-  return custom_packet
-
+  return custom_packet """
 
 def readP(singlepacket):
   # Print IP source and destination
@@ -43,13 +45,9 @@ def readP(singlepacket):
     timestamp = singlepacket.time
     print("IP Source: " + str(ipsource) + " | IP Dest: " + str(ipdest))
     if not EPOCH:
+      timestamp = int(timestamp)
       timestamp = gmtime(timestamp)
-<<<<<<< Updated upstream
       print("Time: " + str(timestamp[3]).zfill(2) + ":" + str(timestamp[4]).zfill(2) + ":" + str(timestamp[5]).zfill(2) + " GMT, " + str(timestamp[1]) + "/" + str(timestamp[2]) + " /" + str(timestamp[0]))
-=======
-      print("Time: " + str(timestamp[3]).zfill(2) + ":" + str(timestamp[4]).zfill(2) + ":" + str(
-          timestamp[5]).zfill(2) + " GMT, " + str(timestamp[1]) + "/" + str(timestamp[2]) + " /" + str(timestamp[0]))
->>>>>>> Stashed changes
     elif EPOCH:
       print("Epoch Time: " + str(timestamp))
 
@@ -58,8 +56,7 @@ def readP(singlepacket):
   if TCP in singlepacket:
     tcpsourceport = singlepacket[TCP].sport
     tcpdestport = singlepacket[TCP].dport
-    print("TCP Source Port: " + str(tcpsourceport) +
-          " | TCP Dest Port: " + str(tcpdestport))
+    print("TCP Source Port: " + str(tcpsourceport) + " | TCP Dest Port: " + str(tcpdestport))
 
     # Print HTTP Request Type
     # Check if HTTP is present in the packet
@@ -79,17 +76,15 @@ def readP(singlepacket):
   if DNSRR in singlepacket:
     hostaddr = singlepacket[DNSRR].rdata
     #recordtype = singlepacket[DNSRR].qtype
-    # + " | TCP Dest Port: " + str(recordtype))
-    print("DNS Host Address: " + str(hostaddr))
-
+    print("DNS Host Address: " + str(hostaddr)) #+ " | TCP Dest Port: " + str(recordtype))
+  
   # Print UDP source port and UDP destination port
   # Check if UDP is present in the packet
   if UDP in singlepacket:
     udpsrcport = singlepacket[UDP].sport
     udpdestport = singlepacket[UDP].dport
-    print("UDP Source Port: " + str(udpsrcport) +
-          " | UDP Dest Port: " + str(udpdestport))
-
+    print("UDP Source Port: " + str(udpsrcport) + " | UDP Dest Port: " + str(udpdestport))
+  
   # Print ICMP Type:
   # Check if ICMP is present in the packet
   if ICMP in singlepacket:
@@ -97,29 +92,49 @@ def readP(singlepacket):
     print("ICMP Type: " + str(icmptype))
 
 # Method to suggest snort rules based on packet information
-
-
-def RuleMaker(singlepacket) -> list:
+def RuleMaker(singlepacket):
   suggestion = ""
   rule_list = []
+  #Scan list dictionary - IP : list containing port-timestamps
+
+  #ICMP Rules
 
   #TCP Rules
   if TCP in singlepacket:
     tcpsourceport = singlepacket[TCP].sport
     tcpdestport = singlepacket[TCP].dport
+    tcpsourceaddr = singlepacket[IP].src
+    #tcpdestaddr = singlepacket[IP].dst
+    window_size = singlepacket[TCP].window
+    str_port_add = ""
 
     #Checks if suspicious ports
     if tcpsourceport in BAD_PORTS:
-      suggestion = "drop TCP " + str(singlepacket[IP].src) + ' ' + str(
-          tcpsourceport) + ' -> any any (msg: "Suspicious activity on Port ' + str(tcpsourceport) + '"; sid ' + str(SID_START + 1) + ")\n"
+      suggestion = "drop TCP " + str(singlepacket[IP].src) + ' ' + str(tcpsourceport) +' -> any any (msg: "Suspicious activity on Port ' + str(tcpsourceport) + '"; sid ' + str(SID_START + 1) + ")\n"
       rule_list.append(suggestion)
     if tcpdestport == BAD_PORTS:
-      suggestion = "drop TCP any any -> " + str(singlepacket[IP].src) + ' ' + str(
-          tcpdestport) + ' (msg: "Suspicious Port activity on  ' + str(tcpdestport) + '"; sid ' + str(SID_START + 2) + ")\n"
+      suggestion = "drop TCP any any -> " + str(singlepacket[IP].src) + ' ' + str(tcpdestport) + ' (msg: "Suspicious Port activity on  ' + str(tcpdestport) + '"; sid ' + str(SID_START + 2) + ")\n"
+      rule_list.append(suggestion)
+    
+    #Checks for a variety of ports being pinged too often (NMAP scan)
+    #Adds packet to dictionary
+    str_port_add = (str(tcpdestport) + "-" + str(singlepacket[IP].time))
+    if tcpsourceaddr in pinged_port_list:
+      pinged_port_list[tcpsourceaddr].append(str_port_add)
+    else: pinged_port_list[tcpsourceaddr] = [str_port_add]
+
+      #Every 5 packets, checks if 
+    #Checks for swapped DHCP ports
+    #Checks for unencrypted traffic
+    #Checks for failed logins
+    #Checks for numerous/frequent requests
+      
+    #Checks for window sizes
+    if window_size > 65535:
+      suggestion = ""
       rule_list.append(suggestion)
   #Return the rules
   return rule_list
-
 
 def HelperMethods(pcap):
   counter = 0
@@ -137,30 +152,24 @@ def HelperMethods(pcap):
       elif rule in snort_rules:
         occurences[(snort_rules.index(rule))] += 1
 
-    counter += 1
+    counter +=1
     print("\n\n")
   print("Snort Rule Suggestions: ")
   for index in range(0, len(snort_rules), 1):
     print(snort_rules[index])
     print("# of Packets Flagged: " + str(occurences[index]))
     print("______________________________________________")
-  for packet in pcap:
-    packet_list.append(loadP(packet))
-<<<<<<< Updated upstream
+"""   for packet in pcap:
+    packet_list.append(loadP(packet)) """
   
-  print(packet_list)
+print(packet_list)
   
-=======
-  for item in packet_list:
-    print(item)
-    input()
-
->>>>>>> Stashed changes
+  
 
 ### MAIN FUNCTION ###
 def main():
   #file_name = sys.argv[1]
-  file_name = "1337 nc.pcap"
+  file_name = "Remote_NMAP.pcap"
   #file_name = "Project test.pcapng"
   # Check if pcap file exists
   # if os.path.isfile(file_name):
@@ -172,9 +181,18 @@ def main():
   #open_file = open(file_name)
   HelperMethods(scapy_cap)
 
+  #debug dictionary printing
+  ppl_keys = list(pinged_port_list)
+  ppl_vals = list(pinged_port_list.values())
+  ppl_len = len(pinged_port_list)
+  debug_counter = 0
+  while debug_counter < ppl_len:
+    print("\n" + "_____________" + "\n")
+    print(str(ppl_keys[debug_counter]) + " : " + str(ppl_vals[debug_counter]))
+    debug_counter +=1
+
   #open_file.close()
-
-
+    
 ### DUNDER CHECK ###
 if __name__ == "__main__":
   main()
